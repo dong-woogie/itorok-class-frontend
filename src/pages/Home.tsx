11dc58bef82560c446d0c367891f44b776da098a
dong-woogie <com6511@gmail.com>
2021-06-11 02:17:40 +0900

    use query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import React from "react";
+import Logo from "../image/logo.png";
+
+const QUERY = gql`
+  query itorok {
+    itorok
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const { data } = useQuery(QUERY);
+  return (
+    <div>
+      {data?.itorok}
+      <img alt="test" src={Logo} />
+    </div>
+  );
 }
 
 export default Home;
