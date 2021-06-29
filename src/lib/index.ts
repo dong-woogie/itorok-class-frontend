445d4f57537a3483cdff8cab9abc200cea0f7bc9
dong-woogie <com6511@gmail.com>
2021-06-29 17:00:03 +0900

    refactoring : 하드코딩 수정

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+export const socialLoginUrl = {
+  kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`,
+}
