6e28da929fb1500b73ef7cec5b06a49dc7c4a606
dong-woogie <com6511@gmail.com>
2021-07-02 14:36:20 +0900

    modify file path

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-
-export interface HeaderProps {
-  isLogged: boolean
-}
+import React, { useCallback } from 'react'
+import { useHistory } from 'react-router'
 
 function Header() {
+  const history = useHistory()
+  const onClickLogo = useCallback(() => {
+    history.push('/')
+  }, [])
   return (
     <div className="base-wrap">
       <div className="px-4 py-2 flex justify-between">
-        <h1 className=" text-2xl text-gray-800 font-bold italic tracking-wider">ITOROK</h1>
+        <h1 onClick={onClickLogo} className="text-2xl text-gray-800 font-bold italic tracking-wider">
+          ITOROK
+        </h1>
         <div className="bg-emerald-400 px-4 flex items-center justify-center text-white font-semibold rounded-md">
           가입하기
         </div>
