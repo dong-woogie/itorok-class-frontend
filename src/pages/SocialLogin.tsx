3e42a791b3005cfe1a76459f9bfba0bf2511779a
dong-woogie <com6511@gmail.com>
2021-07-13 17:07:59 +0900

    social-login 페이지 제거 및 소셜로그인 및 회원가입 graphql 코드 제거

--- src/pages/SocialLogin.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SocialLoginContainer from '../containers/login/SocialLoginContainer'
-
-function SocialLogin() {
-  return <SocialLoginContainer />
-}
-
-export default SocialLogin
