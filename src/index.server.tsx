d293311cd81289c10c220eb10a565a3fe74ab1f1
dong-woogie <com6511@gmail.com>
2021-07-23 23:59:38 +0900

    ssr 일때 apollo client link

--- src/index.server.tsx
+++ src/index.server.tsx
@@ -7,13 +7,16 @@ import path from 'path'
 import serveStatic from 'serve-static'
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
 import { getDataFromTree } from '@apollo/client/react/ssr'
-import fetch from 'node-fetch'
 import { FilledContext, HelmetProvider } from 'react-helmet-async'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './App'
 import rootReducer from './modules'
 import { htmlTemplate } from './server/html'
+import { createApolloClient } from './lib/apollo'
+import { getAccessToken } from './lib/api'
+import fetch from 'node-fetch'
+import { authLink, errorLink, httpLink } from './lib/apollo/link'
 
 const app = express()
 const PORT = 3000
@@ -24,16 +27,12 @@ app.use(serveStatic(path.resolve('./build'), { index: false }))
 
 app.get('*', async (req, res, next) => {
   const extractor = new ChunkExtractor({ statsFile, publicPath: '/' })
+  const { accessToken } = await getAccessToken(req.headers.cookie)
+
   const client = new ApolloClient({
     ssrMode: true,
-    link: createHttpLink({
-      uri: 'http://localhost:4000/graphql',
-      fetch: fetch as any,
-      headers: {
-        cookie: req.headers.cookie || '',
-      },
-    }),
-    cache: new InMemoryCache(),
+    link: errorLink.concat(authLink(accessToken, req.headers.cookie)).concat(httpLink(true)),
+    cache: new InMemoryCache({}),
   })
 
   const helmetContext = {} as FilledContext
@@ -53,7 +52,7 @@ app.get('*', async (req, res, next) => {
 
   try {
     await getDataFromTree(jsx)
-  } catch (e) {}
+  } catch {}
 
   const content = renderToString(jsx)
   const apolloState = client.extract()
