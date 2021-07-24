896ecee1a0b65860c3e93afc41f1cbc6f1d3d3d8
dong-woogie <com6511@gmail.com>
2021-07-24 23:58:51 +0900

    로그인했을 때는 프로필 이미지 렌더

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,20 +1,32 @@
 import React, { useCallback } from 'react'
 import { useHistory } from 'react-router'
+import { useUser } from '../../lib/hooks/useUser'
 
 function Header() {
   const history = useHistory()
   const onClickLogo = useCallback(() => {
     history.push('/')
   }, [])
+  const { data } = useUser()
   return (
-    <div className="base-wrap">
+    <div className="border-b">
       <div className="px-4 py-2 flex justify-between">
-        <h1 onClick={onClickLogo} className="text-2xl text-gray-800 font-bold italic tracking-wider">
+        <div />
+        <h1 onClick={onClickLogo} className="text-2xl text-gray-800 font-bold font-serif tracking-wider cursor-pointer">
           ITOROK
         </h1>
-        <div className="bg-emerald-400 px-4 flex items-center justify-center text-white font-semibold rounded-md">
-          클래스 가입하기
-        </div>
+
+        {data ? (
+          <div className="flex items-center">
+            <div className="h-8 w-8 rounded-full cursor-pointer">
+              <img src={data?.getUserOnLoad.profile.thumbnail} alt="profile" className="w-full h-full rounded-full" />
+            </div>
+          </div>
+        ) : (
+          <div className="bg-emerald-400 px-4 flex items-center justify-center text-white font-semibold rounded-md">
+            클래스 가입하기
+          </div>
+        )}
       </div>
     </div>
   )
