957418bb88326dd2eae30b2e7c47f33546f503b3
dong-woogie <com6511@gmail.com>
2021-07-24 23:58:18 +0900

    Footer style 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
-import { HomeIcon, SearchIcon, BellIcon, UserIcon, ChatIcon } from '@heroicons/react/outline'
-import { Link } from 'react-router-dom'
+import { HomeIcon, SearchIcon, BellIcon, UserIcon, ChatIcon } from '@heroicons/react/solid'
+import { Link, useLocation } from 'react-router-dom'
 
 function Footer() {
+  const location = useLocation()
+  const highlightColorToMatchPath = (pathname: string) => {
+    return location.pathname === pathname
+      ? 'text-gray-800 opacity-100 font-medium'
+      : 'text-gray-400 opacity-70 font-extralight'
+  }
+
   return (
-    <div className="absolute bottom-0 left-0 px-4 py-2 border-t-2 w-full">
-      <div className="flex justify-between">
-        <HomeIcon className="h-8 w-8 text-gray-400" />
-        <SearchIcon className="h-8 w-8 text-gray-400" />
-        <ChatIcon className="h-8 w-8 text-gray-400" />
-        <BellIcon className="h-8 w-8 text-gray-400" />
-        <Link to="/login">
-          <UserIcon className="h-8 w-8 text-gray-400" />
+    <div className="fixed w-full bottom-0 left-0">
+      <div className="max-w-md mx-auto px-4 py-2 border-t flex justify-between">
+        <Link to="/">
+          <div className={`relative flex flex-col items-center ${highlightColorToMatchPath('/')}`}>
+            <HomeIcon className="h-7 w-7" />
+            <span className="text-xs">홈</span>
+          </div>
         </Link>
+        <div className="relative flex flex-col items-center">
+          <SearchIcon className={`h-7 w-7 ${highlightColorToMatchPath('/unknown')}`} />
+          <span className="font-extralight text-xs">검색</span>
+        </div>
+
+        <div className="relative flex flex-col items-center">
+          <ChatIcon className={`h-7 w-7 ${highlightColorToMatchPath('/unknown')}`} />
+          <span className="font-extralight text-xs">채팅</span>
+        </div>
+
+        <div className="relative flex flex-col items-center">
+          <BellIcon className={`h-7 w-7 ${highlightColorToMatchPath('/unknown')}`} />
+          <span className="font-extralight text-xs">알림</span>
+        </div>
+
+        <div className="relative flex flex-col items-center">
+          <UserIcon className={`h-7 w-7 ${highlightColorToMatchPath('/unknown')}`} />
+          <span className="font-extralight text-xs">내정보</span>
+        </div>
       </div>
     </div>
   )
