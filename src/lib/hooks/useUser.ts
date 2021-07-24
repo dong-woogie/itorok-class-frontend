f5e747a29a2b1866dd71ea6137ae4f8432166bec
dong-woogie <com6511@gmail.com>
2021-07-24 23:39:23 +0900

    return 값 수정

--- src/lib/hooks/useUser.ts
+++ src/lib/hooks/useUser.ts
@@ -3,7 +3,5 @@ import { getUserOnLoad } from '../../__generated__/getUserOnLoad'
 import { GET_USER_ON_LOAD } from '../graphql'
 
 export function useUser() {
-  const { data } = useQuery<getUserOnLoad>(GET_USER_ON_LOAD)
-
-  return { data }
+  return useQuery<getUserOnLoad>(GET_USER_ON_LOAD)
 }
