cfdc6293577e48ae610398ce43fd7e21f88b2959
dong-woogie <com6511@gmail.com>
2021-07-26 23:50:35 +0900

    rename

--- src/components/common/Overlay.tsx
+++ src/components/common/Overlay.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { XIcon } from '@heroicons/react/outline'
 
-interface PopoverProps {
+interface OverlayProps {
   children: React.ReactElement
   title: string
   onClose: () => void
 }
 
-function Popover({ children, title, onClose }: PopoverProps) {
+function Overlay({ children, title, onClose }: OverlayProps) {
   return (
-    <div className="fixed top-0 z-10 w-full min-h-screen">
+    <div className="fixed bg-white top-0 z-10 w-full min-h-screen">
       <div className="w-full flex justify-between items-center px-4 py-2 border-b-2">
         <div />
         <h3 className="text-lg">{title}</h3>
@@ -22,4 +22,4 @@ function Popover({ children, title, onClose }: PopoverProps) {
   )
 }
 
-export default Popover
+export default Overlay
