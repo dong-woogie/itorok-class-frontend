d5e1309e0e57d448ed14563a403c67209826644a
dong-woogie <com6511@gmail.com>
2021-07-28 23:51:44 +0900

    update Button Component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 interface ButtonProps {
   text?: string
   onClick?: () => void
-  size?: 'small' | 'medium'
+  size?: 'small' | 'medium' | 'full'
   canClick?: boolean
 }
 
 function Button({ text, onClick, canClick = true, size = 'medium', ...rest }: ButtonProps) {
   const buttonSize = {
-    small: 'py-2 font-medium text-base',
-    medium: 'py-3 font-semibold text-lg',
+    small: 'py-2 font-medium text-base rounded-md',
+    medium: 'py-3 font-semibold text-lg rounded-md',
+    full: 'font-medium text-base w-full py-3',
   }
 
   const isCanClickStyle = canClick
@@ -19,7 +20,7 @@ function Button({ text, onClick, canClick = true, size = 'medium', ...rest }: Bu
   return (
     <button
       onClick={onClick}
-      className={`${buttonSize[size]} w-full text-white rounded-md outline-none focus:outline-none ${isCanClickStyle}`}
+      className={`${buttonSize[size]} w-full text-white outline-none focus:outline-none ${isCanClickStyle}`}
       {...rest}
     >
       {text}
