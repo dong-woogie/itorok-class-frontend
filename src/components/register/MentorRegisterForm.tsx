10fb2648a51165f59249511894c3826b9b5adbc6
dong-woogie <com6511@gmail.com>
2021-08-02 19:56:40 +0900

    멘토일 때 회원가입 프로세스 구현

---
+++ src/components/register/MentorRegisterForm.tsx
@@ -0,0 +1,186 @@
+import React, { useState, useCallback, useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import NumberFormat from 'react-number-format'
+import { useDispatch } from 'react-redux'
+import { openAddressDialog } from '../../modules/dialog'
+import { Gender, RegisterWithSocialInput, UserRole } from '../../__generated__/globalTypes'
+import Button from '../common/Button'
+import FixedWrap from '../common/FixedWrap'
+import Input from '../common/Input'
+import { ProfileType } from './RegisterForm'
+import SelectGender from './SelectGender'
+
+interface MentorRegisterFormProps {
+  defaultValues: Partial<ProfileType>
+  sendCode: (phoneNumber: string) => Promise<boolean>
+  verifyCode: (body: { phoneNumber: string; code: string }) => Promise<boolean>
+  onSubmit: (profile: RegisterWithSocialInput) => void
+  error: string
+}
+
+interface MentorProfileType {
+  username: string
+  displayName: string
+  shortBio: string
+  phoneNumber: string
+  code: string
+}
+
+function MentorRegisterForm({ defaultValues, sendCode, verifyCode, onSubmit, error }: MentorRegisterFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    getValues,
+  } = useForm<MentorProfileType>({
+    mode: 'onChange',
+    defaultValues: {
+      ...defaultValues,
+      shortBio: `안녕하세요. 멘토 ${defaultValues.displayName}입니다.`,
+    },
+  })
+  const [gender, setGender] = useState<Gender>(Gender.male)
+  const [isCertified, setIsCertified] = useState<boolean>(false)
+  const [isSentCode, setIsSentCode] = useState<boolean>(false)
+
+  const dispatch = useDispatch()
+  const addressRef = useRef<HTMLInputElement>()
+  const detailAddressRef = useRef<HTMLInputElement>()
+
+  const onChangeGender = useCallback((selectGender: Gender) => {
+    setGender(selectGender)
+  }, [])
+  const onChangeAddress = (roadAddress: string) => {
+    if (!addressRef.current) return
+    addressRef.current.value = roadAddress
+    detailAddressRef.current?.focus()
+  }
+  const onFocusAddressInput = () => {
+    dispatch(openAddressDialog({ onComplete: onChangeAddress }))
+  }
+
+  const phoneFormErrorMessage = () => {
+    return errors.phoneNumber?.message || (errors.phoneNumber?.type === 'pattern' && '휴대전화 번호를 입력해주세요.')
+  }
+
+  const onClickSendCode = async () => {
+    const { phoneNumber } = getValues()
+    if (!phoneNumber) return
+    const success = await sendCode(phoneNumber.replace(/-/g, ''))
+    if (success) setIsSentCode(true)
+  }
+
+  const onClickVerifyCode = async () => {
+    const { phoneNumber, code } = getValues()
+
+    const success = await verifyCode({ phoneNumber: phoneNumber.replace(/-/g, ''), code })
+    if (success) setIsCertified(true)
+  }
+
+  const onSubmitForm = handleSubmit(() => {
+    if (!isCertified) return
+    const { username, shortBio, phoneNumber, displayName } = getValues()
+    onSubmit({
+      username,
+      displayName,
+      role: UserRole.mentor,
+      shortBio,
+      phoneNumber,
+      address: addressRef.current?.value || '',
+      detailAddress: detailAddressRef?.current?.value || '',
+      gender,
+    })
+  })
+
+  return (
+    <form className="mt-5 pb-16" onSubmit={onSubmitForm}>
+      <Input
+        label="아이디"
+        placeholder="아이디를 입력해주세요."
+        error={errors.username?.message}
+        {...register('username', { required: '아이디를 입력해주세요' })}
+      />
+      <Input
+        label="사용닉네임"
+        placeholder="닉네임을 입력해주세요."
+        error={errors.displayName?.message}
+        {...register('displayName', { required: '닉네임을 입력해주세요.' })}
+      />
+
+      <SelectGender gender={gender} onChange={onChangeGender} />
+
+      <div className="relative">
+        <NumberFormat
+          customInput={Input}
+          label="휴대번호 인증하기"
+          placeholder="예) 010-xxxx-xxxx"
+          format="###-####-####"
+          {...register('phoneNumber', {
+            required: '휴대전화 번호를 입력해주세요.',
+            pattern: /^[0-9]{3}[-]+[0-9]{4}[-]+[0-9]{4}$/,
+          })}
+          error={phoneFormErrorMessage()}
+          readOnly={isCertified}
+        />
+        {!isCertified && (
+          <div className="absolute right-0 top-0 h-full flex items-center w-20">
+            <Button
+              text={isSentCode ? '재전송' : '전송'}
+              size="xs"
+              onClick={onClickSendCode}
+              type="button"
+              canClick={!phoneFormErrorMessage() && !!getValues().phoneNumber && !isCertified}
+            />
+          </div>
+        )}
+      </div>
+
+      {isSentCode && (
+        <div className="relative">
+          <NumberFormat
+            customInput={Input}
+            format="####"
+            label="인증번호"
+            placeholder="SMS 인증번호를 입력해주세요."
+            {...register('code', {
+              required: '인증번호를 입력해주세요.',
+            })}
+            readOnly={isCertified}
+          />
+          {!isCertified && (
+            <div className="absolute right-0 top-0 h-full flex items-center w-20">
+              <Button text="인증하기" size="xs" type="button" onClick={onClickVerifyCode} canClick={!isCertified} />
+            </div>
+          )}
+        </div>
+      )}
+
+      <Input
+        name="address"
+        label="공방 주소"
+        placeholder="클릭해서 주소를 입력해주세요."
+        ref={addressRef}
+        required
+        readOnly
+        onClick={onFocusAddressInput}
+      />
+
+      <Input
+        name="detailAddress"
+        label="상세 주소"
+        placeholder="상세주소를 입력해주세요."
+        ref={detailAddressRef}
+        required
+      />
+
+      <Input label="한줄 소개" placeholder="한줄로 소개해주세요." {...register('shortBio')} />
+
+      <FixedWrap>
+        <p className="text-rose-400 text-center font-semibold">{error}</p>
+        <Button text="멘토로 가입하기" size="full" canClick={isValid && isCertified && !error} />
+      </FixedWrap>
+    </form>
+  )
+}
+
+export default MentorRegisterForm
--- src/containers/register/MentorRegisterFormContainer.tsx
+++ src/containers/register/MentorRegisterFormContainer.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import { useApolloClient, useMutation, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router'
 import MentorRegisterForm from '../../components/register/MentorRegisterForm'
 import RegisterFormWrap from '../../components/register/RegisterFormWrap'
+import api from '../../lib/api'
+import { splitLink } from '../../lib/apollo/link'
+import { GET_SOCIAL_PROFILE, REGISTER_WITH_SOCIAL } from '../../lib/graphql'
+import { setUser } from '../../modules/user'
+import { GetSocialProfile } from '../../__generated__/GetSocialProfile'
+import { RegisterWithSocialInput } from '../../__generated__/globalTypes'
+import {
+  registerWithSocialMutation,
+  registerWithSocialMutationVariables,
+} from '../../__generated__/registerWithSocialMutation'
 
 function MentorRegisterFormContainer() {
+  const { data: socialProfileData } = useQuery<GetSocialProfile>(GET_SOCIAL_PROFILE, { fetchPolicy: 'network-only' })
+  const [error, setError] = useState<string>('')
+  const history = useHistory()
+  const dispatch = useDispatch()
+  const client = useApolloClient()
+
+  const onCompleted = ({ registerWithSocial }: registerWithSocialMutation) => {
+    if (registerWithSocial.ok) {
+      if (registerWithSocial.accessToken) client.setLink(splitLink(registerWithSocial.accessToken))
+      if (registerWithSocial.user) dispatch(setUser(registerWithSocial.user))
+      return history.push('/')
+    }
+    if (registerWithSocial.error) return setError(registerWithSocial.error)
+    return history.push('/login')
+  }
+
+  const [registerWithSocial] = useMutation<registerWithSocialMutation, registerWithSocialMutationVariables>(
+    REGISTER_WITH_SOCIAL,
+    { onCompleted },
+  )
+
+  const sendCode = async (phoneNumber: string) => {
+    const { data } = await api.post('/api/send-code', { phoneNumber })
+    return data.ok
+  }
+  const verifyCode = async (body: { phoneNumber: string; code: string }) => {
+    const { data } = await api.post('/api/verify-code', body)
+    return data.ok
+  }
+
+  const onSubmit = (profile: RegisterWithSocialInput) => {
+    if (!profile.address) return setError('주소를 입력해주세요.')
+    return registerWithSocial({
+      variables: { input: profile },
+    })
+  }
+
   return (
     <RegisterFormWrap title="멘토 회원가입" description="회원정보를 입력해주세요.">
-      <MentorRegisterForm defaultValues={{}} />
+      {socialProfileData && (
+        <MentorRegisterForm
+          defaultValues={{
+            displayName: socialProfileData?.getSocialProfile?.profile?.displayName || '',
+            username: socialProfileData?.getSocialProfile?.profile?.username || '',
+            shortBio: socialProfileData?.getSocialProfile?.profile?.shortBio || '',
+          }}
+          sendCode={sendCode}
+          verifyCode={verifyCode}
+          onSubmit={onSubmit}
+          error={error}
+        />
+      )}
     </RegisterFormWrap>
   )
 }
