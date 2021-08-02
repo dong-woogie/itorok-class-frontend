5ad1e65db0dc82d698e25abbf5b9fe5f753d6f49
dong-woogie <com6511@gmail.com>
2021-08-02 19:51:55 +0900

    update Button Component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
-interface ButtonProps {
+interface ButtonProps extends Omit<React.HTMLProps<HTMLButtonElement>, 'size'> {
   text?: string
-  onClick?: () => void
-  size?: 'small' | 'medium' | 'full'
+  size?: 'xs' | 'sm' | 'md' | 'full'
   canClick?: boolean
 }
 
-function Button({ text, onClick, canClick = true, size = 'medium', ...rest }: ButtonProps) {
+function Button({ text, canClick = true, size = 'md', ...rest }: ButtonProps) {
   const buttonSize = {
-    small: 'py-2 font-medium text-base rounded-md',
-    medium: 'py-3 font-semibold text-lg rounded-md',
+    xs: 'py-1 px-3 text-sm rounded-sm',
+    sm: 'py-2 font-medium text-base rounded-md',
+    md: 'py-3 font-semibold text-lg rounded-md',
     full: 'font-medium text-base w-full py-3',
   }
+  const htmlProps = rest as any
 
   const isCanClickStyle = canClick
     ? 'bg-emerald-400 active:bg-emerald-600 hover:bg-emerald-500'
     : 'bg-gray-300 pointer-events-none'
   return (
     <button
-      onClick={onClick}
       className={`${buttonSize[size]} w-full text-white outline-none focus:outline-none ${isCanClickStyle}`}
-      {...rest}
+      {...htmlProps}
     >
       {text}
     </button>
