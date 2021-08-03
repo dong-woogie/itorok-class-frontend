81faf3efb0f881161aa8667221c5fe9ca3b9a53e
dong-woogie <com6511@gmail.com>
2021-08-03 23:51:18 +0900

    add color props

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,20 +4,23 @@ interface ButtonProps extends Omit<React.HTMLProps<HTMLButtonElement>, 'size'> {
   text?: string
   size?: 'xs' | 'sm' | 'md' | 'full'
   canClick?: boolean
+  color?: string
 }
 
-function Button({ text, canClick = true, size = 'md', ...rest }: ButtonProps) {
+function Button({ text, canClick = true, size = 'md', color = 'emerald', ...rest }: ButtonProps) {
   const buttonSize = {
     xs: 'py-1 px-3 text-sm rounded-sm',
-    sm: 'py-2 font-medium text-base rounded-md',
+    sm: 'py-1.5 font-medium text-base rounded-md',
     md: 'py-3 font-semibold text-lg rounded-md',
     full: 'font-medium text-base w-full py-3',
   }
   const htmlProps = rest as any
 
-  const isCanClickStyle = canClick
-    ? 'bg-emerald-400 active:bg-emerald-600 hover:bg-emerald-500'
-    : 'bg-gray-300 pointer-events-none'
+  const bgColor = () => {
+    return `bg-${color}-400 hover:bg-${color}-500 active:bg-${color}-600`
+  }
+
+  const isCanClickStyle = canClick ? bgColor() : 'bg-gray-300 pointer-events-none'
   return (
     <button
       className={`${buttonSize[size]} w-full text-white outline-none focus:outline-none ${isCanClickStyle}`}
